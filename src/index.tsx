3130641543740c67710ac0b8f0649665ee7eabab
furfio <1722941803@1722941803@qq.com>
2021-08-01 23:16:18 +0800

    add mind

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Mind from "./mind";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Mind />
   </React.StrictMode>,
   document.getElementById('root')
 );
